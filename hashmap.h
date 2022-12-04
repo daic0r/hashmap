d544127c086295971ed3fae2b51a7d63f2950a8f
daic0r <daiceman@gmx.net>
2022-12-04 20:58:11 +0100

    Implemented iterator

--- hashmap.h
+++ hashmap.h
@@ -15,19 +15,114 @@
 
 template<typename Key, typename Value, typename Hash=std::hash<Key>> 
 class hashmap {
-    struct node_t {
-        Key key;
-        Value value;
-        node_t* pNext{};
-    };
-
+    struct node_t;
     using bucket_t = node_t*;
 
-    std::size_t m_nCapacityLeft{};
-    std::size_t m_nCapacity{};
-    bucket_t* m_arBuffer{};
+public:
+    struct sentinel{};
+    class iterator {
+        friend class hashmap;
+    public:
+        using iterator_concept = std::bidirectional_iterator_tag;
+        using difference_type = std::ptrdiff_t;
+        using value_type = std::remove_cvref_t<Value>;
+        using pointer = value_type*;
+        using reference = value_type&;
+
+        constexpr iterator() = default;
+        constexpr bool operator==(const iterator& other) const noexcept {
+            return m_pHashmap == other.m_pHashmap && m_pNode == other.m_pNode;
+        }
+        constexpr bool operator!=(const iterator& other) const noexcept {
+            return not (*this == other);
+        }
+
+        constexpr reference operator *() const noexcept {
+            return m_pNode->kvp.second;
+        }
+
+        constexpr reference operator->() const noexcept {
+            return *(*this);
+        }
+        constexpr iterator& operator++() noexcept {
+            m_pNode = m_pNode->pNext;
+            if (m_pNode)
+                return *this;
+            ++m_pBucket;
+            ++m_nBucketIndex;
+            while (m_nBucketIndex < m_pHashmap->m_nCapacity && not *m_pBucket) {
+                ++m_pBucket;
+                ++m_nBucketIndex;
+            }
+            m_pNode = *m_pBucket;
+            return *this;
+        }
+        constexpr iterator operator++(int) const noexcept {
+            iterator copy{ *this };
+            ++(*this);
+            return copy;
+        }
+        constexpr iterator& operator--() noexcept {
+            m_pNode = m_pNode->pPrev;
+            if (m_pNode)
+                return *this;
+            if (m_nBucketIndex == 0)
+                return *this;
+            --m_pBucket;
+            --m_nBucketIndex;
+            while (m_nBucketIndex > 0 && not *m_pBucket) {
+                --m_pBucket;
+                --m_nBucketIndex;
+            }
+            m_pNode = *m_pBucket;
+            return *this;
+        }
+        constexpr iterator operator--(int) const noexcept {
+            iterator copy{ *this };
+            --(*this);
+            return copy;
+        }
+
+
+        constexpr bool operator==(sentinel) const noexcept {
+            return m_nBucketIndex >= m_pHashmap->m_nCapacity;
+        }
+        constexpr bool operator!=(sentinel) const noexcept {
+            return not (*this == sentinel{});
+        }
+
+    private:
+        constexpr iterator(hashmap& hm, bucket_t* pBucket, node_t* pNode, std::optional<std::size_t> nBucketIdx) : 
+            m_pHashmap{&hm},
+            m_pBucket{pBucket ? pBucket : hm.m_arBuffer},
+            m_nBucketIndex{pBucket ? nBucketIdx.value() : 0},
+            m_pNode{pNode}
+        {
+            if (not pNode) {
+                while (not *m_pBucket && m_nBucketIndex < hm.m_nCapacity) {
+                    ++m_pBucket;
+                    ++m_nBucketIndex;
+                }
+                if (*m_pBucket)
+                    m_pNode = *m_pBucket;
+            }
+        }
+        constexpr iterator(hashmap& hm, sentinel) :
+            m_pHashmap{&hm}
+        {
+            m_nBucketIndex = hm.m_nCapacity;
+        }
+
+    private:
+        hashmap* m_pHashmap{};
+        bucket_t* m_pBucket{};
+        node_t* m_pNode{};
+        std::size_t m_nBucketIndex{};
+    };
 
 public:
+    static_assert(std::bidirectional_iterator<iterator>);
+
     constexpr hashmap() : m_arBuffer{ 
         m_nCapacity > 0 ? new bucket_t[m_nCapacity] : nullptr }
     {}
@@ -69,7 +164,7 @@ public:
     constexpr auto size() const noexcept { return m_nCapacity - m_nCapacityLeft; }
 
     template<std::convertible_to<Key> T, std::convertible_to<Value> U>
-    constexpr bool emplace(T&& key, U&& val) {
+    constexpr std::pair<iterator, bool> emplace(T&& key, U&& val) {
         if (m_nCapacityLeft == 0) {
             grow();    
         }
@@ -82,27 +177,39 @@ public:
             pNewNode = &(*pNewNode)->pNext;
         }
         try {
-            *pNewNode = new node_t{ std::forward<T>(key), std::forward<U>(val), nullptr };
+            *pNewNode = new node_t{ std::make_pair(std::forward<T>(key), std::forward<U>(val)), nullptr, nullptr };
         }
         catch (...) {
-            return false;
+            return std::make_pair(iterator{ *this, sentinel{} }, false);
         }
-        if (pParent)
+        if (pParent) {
             pParent->pNext = *pNewNode;
+            (*pNewNode)->pPrev = pParent;
+        }
         if (pNewNode == pBucket)
             --m_nCapacityLeft;
-        return true;
+        const std::size_t nBucktIdx = pBucket - &m_arBuffer[0];
+        return std::make_pair(iterator{ *this, pBucket, *pNewNode, nBucktIdx }, true);
     }
 
     template<std::convertible_to<Key> T>
-    constexpr std::optional<std::reference_wrapper<Value>> find(T&& key) const noexcept {
+    constexpr iterator find(T&& key) const noexcept {
         auto pNode = m_arBuffer[Hash{}(std::forward<T>(key)) % m_nCapacity];
-        while (pNode && (std::forward<T>(key) != pNode->key)) {
+        const auto pBucket = &pNode;
+        while (pNode && (std::forward<T>(key) != pNode->kvp.first)) {
             pNode = pNode->pNext;
         }
         if (not pNode)
-            return std::nullopt;
-        return pNode->value;
+            return iterator{ *this, sentinel{} };
+        return iterator{ *this, pBucket, pNode, pBucket - &m_arBuffer[0] };
+    }
+
+    constexpr auto begin() noexcept {
+        return iterator{ *this, nullptr, nullptr, std::nullopt };
+    }
+
+    constexpr auto end() noexcept {
+        return sentinel{};
     }
 
 private:
@@ -149,7 +256,8 @@ private:
 #endif
             temp.m_arBuffer = reinterpret_cast<bucket_t*>(operator new[](temp.m_nCapacity * sizeof(bucket_t)));
             std::memset(temp.m_arBuffer, 0, temp.m_nCapacity * sizeof(bucket_t));
-            copy_buckets<true>(temp.m_arBuffer, m_arBuffer, m_nCapacity);
+            if (m_arBuffer)
+                copy_buckets<true>(temp.m_arBuffer, m_arBuffer, m_nCapacity);
             temp.swap(*this);
         }
         catch (...) {
@@ -158,6 +266,16 @@ private:
         return true;
     }
 
+private:
+    struct node_t {
+        std::pair<const Key, Value> kvp;
+        node_t* pNext{};
+        node_t* pPrev{};
+    };
+
+    std::size_t m_nCapacityLeft{};
+    std::size_t m_nCapacity{};
+    bucket_t* m_arBuffer{};
 };
 
 #endif
